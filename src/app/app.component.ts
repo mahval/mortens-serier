7ecd6f46bbd6d5e148fe8eef307665f9fe659092
Marianne Hval <mariannehval@gmail.com>
2019-01-22 18:14:07 +0100

    Select content buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TYPES_OF_CONTENT } from './variables';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mortens-serier';
+  types_of_content = TYPES_OF_CONTENT;
+  selectedContent = this.types_of_content[0];
+
+  selectContent(content) {
+    this.selectedContent = content;
+  }
+
+  contentIsSelected(content) {
+    return (content === this.selectedContent);
+  }
 }
---
+++ src/app/variables.ts
@@ -0,0 +1,5 @@
+export const TYPES_OF_CONTENT = [
+    { 'name': 'Striper' },
+    { 'name': 'Historier' },
+    { 'name': 'Helsider' }
+];
