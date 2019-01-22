7b35f432fe6c38abdb2dc10b782c3df3d058c902
Marianne Hval <mariannehval@gmail.com>
2019-01-22 17:43:36 +0100

    Banner and comic card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+
+import { MatCardModule, MatButtonModule, MatIconModule } from '@angular/material';
+
+const appRoutes: Routes = [
+
+  { path: '', component: FrontpageComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  templateUrl: './frontpage.component.html',
+  styleUrls: ['./frontpage.component.scss']
+})
+export class FrontpageComponent implements OnInit {
+
+  now = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
